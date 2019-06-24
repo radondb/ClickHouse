e8860ca39f54d06691d0625ae50a24fc9d482ae4
zhang2014 <coswde@gmail.com>
2019-06-24 15:25:43 +0800

    tmp

--- dbms/src/DataTypes/DataTypeJSON.cpp
+++ dbms/src/DataTypes/DataTypeJSON.cpp
@@ -8,9 +8,55 @@
 #include <Columns/ColumnString.h>
 #include <Functions/FunctionHelpers.h>
 #include <IO/WriteHelpers.h>
+#include <Functions/SimdJSONParser.h>
+#include <Common/CpuId.h>
 
 namespace DB
 {
+
+static inline void dumpJson(SimdJSONParser::Iterator & iterator)
+{
+    if (SimdJSONParser::isObject(iterator)) {
+        std::cout << "{";
+
+        if (SimdJSONParser::firstObjectMember(iterator))
+        {
+            std::cout << SimdJSONParser::getKey(iterator);
+            std::cout << ":";
+            dumpJson(iterator);
+
+            while(SimdJSONParser::nextObjectMember(iterator))
+            {
+                std::cout << ",";
+                std::cout << SimdJSONParser::getKey(iterator);
+                std::cout << ":";
+                dumpJson(iterator);
+            }
+
+            iterator.up();
+        }
+
+        std::cout << "}";
+    } else if (SimdJSONParser::isArray(iterator)) {
+        std::cout << "[";
+
+        if (SimdJSONParser::firstArrayElement(iterator))
+        {
+            dumpJson(iterator);
+            while (SimdJSONParser::nextArrayElement(iterator))
+            {
+                std::cout << ",";
+                dumpJson(iterator);
+            }
+
+            iterator.up();
+        }
+
+        std::cout << "]";
+    } else {
+        iterator.print(std::cout); // just print the lone value
+    }
+}
 
 template <typename Reader>
 static inline void read(IColumn & column, Reader && reader)
@@ -23,6 +69,23 @@ static inline void read(IColumn & column, Reader && reader)
     try
     {
         reader(data);
+
+#if USE_SIMDJSON
+        if (!Cpu::CpuFlagsCache::have_AVX2)
+            throw Exception("Not have avx2.", ErrorCodes::LOGICAL_ERROR);
+
+        SimdJSONParser parser;
+
+        parser.preallocate(data.size() - old_chars_size);
+        std::vector<typename SimdJSONParser::Iterator> stack;
+
+        if (parser.parse(StringRef(data.data() + old_chars_size, data.size() - old_chars_size)))
+        {
+            auto root = parser.getRoot();
+            dumpJson(root);
+            std::cout << "\n";
+        }
+#endif
         data.push_back(0);
         offsets.push_back(data.size());
     }
@@ -68,16 +131,6 @@ public:
         writeCSVString<>(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
     }
 
-    void serializeTextEscaped(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & /*settings*/) const override
-    {
-        writeEscapedString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
-    }
-
-    void serializeTextQuoted(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & /*settings*/) const override
-    {
-        writeQuotedString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
-    }
-
     void serializeTextJSON(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const override
     {
         writeJSONString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr, settings);
@@ -87,6 +140,16 @@ public:
     {
         writeXMLString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
     }
+
+    void serializeTextQuoted(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & /*settings*/) const override
+    {
+        writeQuotedString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
+    }
+
+    void serializeTextEscaped(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & /*settings*/) const override
+    {
+        writeEscapedString(static_cast<const ColumnString &>(column).getDataAt(row_num), ostr);
+    }
 };
 
 
@@ -95,8 +158,7 @@ void registerDataTypeJSON(DataTypeFactory & factory)
     factory.registerSimpleDataTypeCustom("JSON", []
     {
         return std::make_pair(DataTypeFactory::instance().get("String"),
-                              std::make_unique<DataTypeCustomDesc>(std::make_unique<DataTypeCustomFixedName>("JSON"),
-                                                                   std::make_unique<DataTypeCustomJSONSerialization>()));
+            std::make_unique<DataTypeCustomDesc>(std::make_unique<DataTypeCustomFixedName>("JSON"), std::make_unique<DataTypeCustomJSONSerialization>()));
     });
 }
 
